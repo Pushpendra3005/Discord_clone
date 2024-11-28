8c5eae8772ae97d166e59e83fe67ea0ddebbce59
Pushpendra Verma <119849121+Pushpendra3005@users.noreply.github.com>
2024-11-28 15:57:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        lightblue: "#404EED"
+      },
+      fontFamily :{
+        'ggsans': ['ggsans-normal'],
+      },
+      spacing: {
+        '30' : '120px'
+      }
+    },
+  },
+  plugins: [],
+}
+
